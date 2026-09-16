a22c1b14a02e5efef9b17bba0775ed2db3da0073
emiliaeminent-wq <emiliaeminent@gmail.com>
2026-09-16 21:04:20 +0000

    add website files

---
+++ script.js
@@ -0,0 +1,45 @@
+window.addEventListener("scroll", function () {
+
+    let scrollPosition = window.scrollY;
+
+    if (scrollPosition < 300) {
+
+        document.body.style.backgroundColor = "#f5f7fa";
+
+    } else if (scrollPosition < 700) {
+
+        document.body.style.backgroundColor = "#dff6ff";
+
+    } else if (scrollPosition < 1100) {
+
+        document.body.style.backgroundColor = "#fff4cc";
+
+    } else {
+
+        document.body.style.backgroundColor = "#e8dff5";
+
+    }
+
+});window.addEventListener("scroll", function () {
+
+    let scrollPosition = window.scrollY;
+
+    if (scrollPosition < 300) {
+
+        document.body.style.backgroundColor = "#f5f7fa";
+
+    } else if (scrollPosition < 700) {
+
+        document.body.style.backgroundColor = "#dff6ff";
+
+    } else if (scrollPosition < 1100) {
+
+        document.body.style.backgroundColor = "#fff4cc";
+
+    } else {
+
+        document.body.style.backgroundColor = "#e8dff5";
+
+    }
+
+});
