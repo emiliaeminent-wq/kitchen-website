2f09267b0c02ac73c3527b9b944aa1886874fe45
emiliaeminent-wq <emiliaeminent@gmail.com>
2026-09-18 20:14:43 +0000

    Update script.js

--- script.js
+++ script.js
@@ -4,21 +4,32 @@ window.addEventListener("scroll", function () {
 
     let scrollPosition = window.scrollY;
 
-    if (scrollPosition < 300) {
+    let pageHeight =
+        document.documentElement.scrollHeight - window.innerHeight;
+
+    let scrollPercent =
+        (scrollPosition / pageHeight) * 100;
+
+
+    if (scrollPercent < 20) {
 
         document.body.style.backgroundColor = "#f5f7fa";
 
-    } else if (scrollPosition < 700) {
+    } else if (scrollPercent < 40) {
 
         document.body.style.backgroundColor = "#dff6ff";
 
-    } else if (scrollPosition < 1100) {
+    } else if (scrollPercent < 60) {
 
         document.body.style.backgroundColor = "#fff4cc";
 
-    } else {
+    } else if (scrollPercent < 80) {
 
         document.body.style.backgroundColor = "#e8dff5";
+
+    } else {
+
+        document.body.style.backgroundColor = "#d9f7e8";
 
     }
 
